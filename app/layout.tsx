7385c97f646f25431befe99ee0e45df46eae774b
Jarrod Watts <jarrod@cubelabs.xyz>
2025-03-27 11:08:13 +1100

    Initial setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import AbstractWalletWrapper from "@/components/NextAbstractWalletProvider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,11 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <AbstractWalletWrapper>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </AbstractWalletWrapper>
     </html>
   );
 }
